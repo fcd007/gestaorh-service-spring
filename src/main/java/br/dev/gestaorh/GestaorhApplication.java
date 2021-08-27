3fc154e7921c716ea5d7e576484b920d0cf33d6a
supero.fdantas <francisco.dantas@supero.com.br>
2021-08-26 22:07:44 -0300

    add files projeto gestoa rh

---
+++ src/main/java/br/dev/gestaorh/GestaorhApplication.java
@@ -0,0 +1,13 @@
+package br.dev.gestaorh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaorhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaorhApplication.class, args);
+	}
+
+}
